9c7dcc633df7ab02ff44238bfcf86d1ef6628c6b
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:18:00 +0100

    feat: create base EqualityComparer

---
+++ Assets/BetterCommons/Runtime/Comparers/EqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Better.Commons.Runtime.Comparers
+{
+	[Serializable]
+	public abstract class EqualityComparer<T> : IEqualityComparer<T>
+	{
+		public bool Equals(T x, T y)
+		{
+			if (ReferenceEquals(x, y))
+			{
+				return true;
+			}
+
+			if (ReferenceEquals(x, null))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(y, null))
+			{
+				return false;
+			}
+
+			var equaled = EqualsValue(x, y);
+			return equaled;
+		}
+
+		protected abstract bool EqualsValue(T x, T y);
+		
+		public abstract int GetHashCode(T obj);
+	}
+}
