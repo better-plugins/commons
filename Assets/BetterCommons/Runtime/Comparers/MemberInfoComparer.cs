effb9ca8a42f9d8802dfdbd059e933c389e59821
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:19:55 +0100

    feat: create runtime Comparers

---
+++ Assets/BetterCommons/Runtime/Comparers/MemberInfoComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Better.Commons.Runtime.Comparers
+{
+	public class MemberInfoComparer : EqualityComparer<MemberInfo>
+	{
+		protected override bool EqualsValue(MemberInfo x, MemberInfo y)
+		{
+			var equals = x.Name == y.Name && x.DeclaringType == y.DeclaringType;
+			return equals;
+		}
+
+		public override int GetHashCode(MemberInfo obj)
+		{
+			unchecked
+			{
+				var hashCode = obj.Name.GetHashCode();
+
+				if (obj.DeclaringType != null)
+				{
+					var declaringHashCode = obj.DeclaringType.GetHashCode();
+					hashCode = hashCode * 397 ^ declaringHashCode;
+				}
+
+				return hashCode;
+			}
+		}
+	}
+}
