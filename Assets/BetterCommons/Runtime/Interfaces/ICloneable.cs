a13b6908289d14f8aaa7a694ed316e83f62fa42e
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:28:11 +0100

    feat: add base Interfaces

---
+++ Assets/BetterCommons/Runtime/Interfaces/ICloneable.cs
@@ -0,0 +1,7 @@
+﻿namespace Better.Commons.Runtime.Interfaces
+{
+	public interface ICloneable<out T>
+	{
+		public T Clone();
+	}
+}
---
+++ Assets/BetterCommons/Runtime/Interfaces/ICopyable.cs
@@ -0,0 +1,11 @@
+﻿namespace Better.Commons.Runtime.Interfaces
+{
+	public interface ICopyable<in T>
+	{
+		public void Copy(T source);
+	}
+
+	public interface ICopyable : ICopyable<object>
+	{
+	}
+}
