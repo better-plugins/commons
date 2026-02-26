7b8fdd50b516961e6fa6c02cf6ec0266299592aa
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:39:06 +0100

    feat: create Enumerators

---
+++ Assets/BetterCommons/Runtime/Enumerators/StubEnumerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Better.Commons.Runtime.Enumerators
+{
+	public struct StubEnumerator<T> : IEnumerator<T>
+	{
+		public static readonly StubEnumerator<T> Default = new();
+
+		public T Current { get; }
+		object IEnumerator.Current => Current;
+
+		public bool MoveNext()
+		{
+			return false;
+		}
+
+		public void Reset()
+		{
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
