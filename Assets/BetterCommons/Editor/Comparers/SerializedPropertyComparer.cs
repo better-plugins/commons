1e821deef2d8373114019e6a6f13da939a1acdc4
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:20:36 +0100

    feat: create editor Comparers

---
+++ Assets/BetterCommons/Editor/Comparers/SerializedPropertyComparer.cs
@@ -0,0 +1,58 @@
+﻿using Better.Commons.Runtime.Comparers;
+using UnityEditor;
+
+namespace Better.Commons.EditorAddons.Comparers
+{
+	public class SerializedPropertyComparer : EqualityComparer<SerializedProperty>
+	{
+		protected override bool EqualsValue(SerializedProperty x, SerializedProperty y)
+		{
+			// TODO: will be updated with #14
+			/*
+			if (x.IsDisposed())
+			{
+				return false;
+			}
+
+			if (y.IsDisposed())
+			{
+				return false;
+			}
+
+			if (!x.CompareTypes(y))
+			{
+				return false;
+			}
+
+			var pathEquals = x.IsPathEquals(y);
+			return pathEquals;
+			*/
+
+			return false;
+		}
+
+		public override int GetHashCode(SerializedProperty obj)
+		{
+			// TODO: will be updated with #14
+			/*
+			if (!obj.Verify())
+			{
+				return 0;
+			}
+
+			if (obj.IsDisposed())
+			{
+				return 0;
+			}
+
+			if (obj.propertyPath.IsNullOrEmpty())
+			{
+				return 0;
+			}
+			*/
+
+			var pathHashCode = obj.propertyPath.GetHashCode();
+			return pathHashCode;
+		}
+	}
+}
