d60948be2ae261e4a335f24d88cb9b9183856caa
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-26 22:49:05 +0100

    feat: create custom Events

---
+++ Assets/BetterCommons/Runtime/Events/Event.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Better.Commons.Runtime.Events
+{
+	public abstract class Event<TCollection, TDelegate> : IEnumerable<TDelegate>
+		where TCollection : ICollection<TDelegate>, new()
+		where TDelegate : Delegate
+	{
+		protected TCollection Delegates { get; }
+
+		protected Event()
+		{
+			Delegates = new TCollection();
+		}
+
+		public bool Subscribe(TDelegate value)
+		{
+			if (Delegates.Contains(value))
+			{
+				return false;
+			}
+
+			Delegates.Add(value);
+			OnDelegateSubscribed(value);
+			return true;
+		}
+
+		protected virtual void OnDelegateSubscribed(TDelegate value)
+		{
+		}
+
+		public bool Unsubscribe(TDelegate value)
+		{
+			var unsubscribed = Delegates.Remove(value);
+
+			if (unsubscribed)
+			{
+				OnDelegateUnsubscribed(value);
+			}
+
+			return unsubscribed;
+		}
+
+		protected virtual void OnDelegateUnsubscribed(TDelegate value)
+		{
+		}
+
+		IEnumerator<TDelegate> IEnumerable<TDelegate>.GetEnumerator()
+		{
+			var enumerator = CreateEnumerator();
+			return enumerator;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			var enumerator = CreateEnumerator();
+			return enumerator;
+		}
+
+		protected virtual IEnumerator<TDelegate> CreateEnumerator()
+		{
+			var enumerator = Delegates.GetEnumerator();
+			return enumerator;
+		}
+
+		public void Clear()
+		{
+			Delegates.Clear();
+			OnCleared();
+		}
+
+		protected virtual void OnCleared()
+		{
+		}
+
+		public override string ToString()
+		{
+			var value = $"{nameof(Delegates)}({Delegates.Count})";
+			return value;
+		}
+	}
+
+	public abstract class Event<TDelegate> : Event<HashSet<TDelegate>, TDelegate>
+		where TDelegate : Delegate
+	{
+	}
+}
---
+++ Assets/BetterCommons/Runtime/Events/FlatEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Better.Commons.Runtime.Enumerators;
+
+namespace Better.Commons.Runtime.Events
+{
+	public class FlatEvent<TCollection, TDelegate> : Event<TCollection, TDelegate>
+		where TCollection : ICollection<TDelegate>, new()
+		where TDelegate : Delegate
+	{
+		private CancellationTokenSource _enumeratorTokenSource;
+
+		protected override IEnumerator<TDelegate> CreateEnumerator()
+		{
+			var source = base.CreateEnumerator();
+
+			_enumeratorTokenSource?.Cancel();
+			_enumeratorTokenSource = new CancellationTokenSource();
+
+			var enumerator = new CancellableEnumerator<TDelegate>(source, _enumeratorTokenSource.Token);
+			return enumerator;
+		}
+	}
+
+	public class FlatEvent<TDelegate> : FlatEvent<HashSet<TDelegate>, TDelegate>
+		where TDelegate : Delegate
+	{
+	}
+}
