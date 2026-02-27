dd7712213db07e57bb6faf7c444db3108670e7b5
Anatolii Bryzhan <anatolii.bryzhan@technodwarves.com>
2026-02-27 13:31:23 +0100

    feat: create Metadata wrappers

---
+++ Assets/BetterCommons/Editor/Metadata/SerializedObjectMetadata.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+
+namespace Better.Commons.EditorAddons.Metadata
+{
+	public class SerializedObjectMetadata
+	{
+		private readonly SerializedObject _serializedObject;
+		private readonly Dictionary<Type, MemberInfo[]> _memberInfosMap;
+
+		public Editor RootEditor { get; }
+		public Type TargetType { get; }
+
+		public bool IsValid { get; private set; }
+
+		public SerializedObjectMetadata(SerializedObject serializedObject, Editor rootEditor)
+		{
+			_serializedObject = serializedObject;
+			_memberInfosMap = new Dictionary<Type, MemberInfo[]>();
+			RootEditor = rootEditor;
+
+			if (_serializedObject == null)
+			{
+				return;
+			}
+			
+			// TODO: will be updated with #14
+			// if (!_serializedObject.ValidateTargets())
+			// {
+				// return;
+			// }
+
+			foreach (var target in _serializedObject.targetObjects)
+			{
+				var targetType = target.GetType();
+				var allMembers = Array.Empty<MemberInfo>();
+				
+				// TODO: will be updated with #14
+				// var allMembers = targetType.GetMembers(BindingFlagsUtility.MethodFlags);
+
+				if (_memberInfosMap.TryGetValue(targetType, out var members))
+				{
+					allMembers = members.Intersect(allMembers)
+						.ToArray();
+
+					_memberInfosMap[targetType] = allMembers;
+				}
+				else
+				{
+					_memberInfosMap.Add(targetType, allMembers);
+				}
+			}
+
+			// TODO: will be updated with #14
+			// if (!ReflectionUtility.TryGetCommonBaseType(_serializedObject.targetObjects, out var commonBaseType))
+			// {
+				// return;
+			// }
+			Type commonBaseType = null;
+
+			TargetType = commonBaseType;
+			IsValid = true;
+		}
+
+		public SerializedObjectMetadata(SerializedObject serializedObject) : this(serializedObject, null)
+		{
+		}
+
+		public bool TryGetAttributes(out Attribute[] attributes)
+		{
+			if (!Validation(true))
+			{
+				attributes = null;
+				return false;
+			}
+			
+			// TODO: will be updated with #14
+			/*
+			if (!_serializedObject.ValidateTargets())
+			{
+				attributes = null;
+				return false;
+			}
+
+			if (ReflectionUtility.TryGetCommonBaseType(_serializedObject.targetObjects, out var baseType))
+			{
+				attributes = baseType.GetCustomAttributes()
+					.ToArray();
+
+				return true;
+			}
+			*/
+
+			attributes = null;
+			return false;
+		}
+
+		public bool TryGetSerializedProperties(out SerializedProperty[] properties, bool enterChildren = false)
+		{
+			if (!Validation(true))
+			{
+				properties = null;
+				return false;
+			}
+
+			// TODO: will be updated with #14
+			// if (!_serializedObject.ValidateTargets())
+			// {
+				// properties = null;
+				// return false;
+			// }
+
+			var serializedProperties = new List<SerializedProperty>();
+			var iterator = _serializedObject.GetIterator();
+
+			if (iterator.NextVisible(true))
+			{
+				do
+				{
+					var copy = iterator.Copy();
+					serializedProperties.Add(copy);
+				} while (iterator.NextVisible(enterChildren));
+			}
+
+			properties = serializedProperties.ToArray();
+			return true;
+		}
+
+		public bool TryGetMembers<TMember>(out TMember[] memberInfos)
+			where TMember : MemberInfo
+		{
+			if (!Validation(true))
+			{
+				memberInfos = null;
+				return false;
+			}
+
+			// TODO: will be updated with #14
+			// if (!_serializedObject.ValidateTargets())
+			// {
+				// memberInfos = null;
+				// return false;
+			// }
+
+			var bufferMembers = Enumerable.Empty<TMember>();
+
+			foreach (var target in _serializedObject.targetObjects)
+			{
+				var targetType = target.GetType();
+
+				if (!_memberInfosMap.TryGetValue(targetType, out var allMembers))
+				{
+					continue;
+				}
+
+				var members = allMembers.OfType<TMember>();
+				bufferMembers = bufferMembers.Concat(members);
+			}
+
+			memberInfos = bufferMembers.Distinct()
+				.ToArray();
+
+			return true;
+		}
+
+		private bool Validation(bool targetState, bool logException = true)
+		{
+			var isValid = IsValid == targetState;
+
+			if (!isValid && logException)
+			{
+				// TODO: will be updated with #14
+				// var reason = targetState ? "must be valid" : "must be invalid";
+				// var message = "Improper state, " + reason;
+				// DebugUtility.LogException<InvalidOperationException>(message);
+			}
+
+			return isValid;
+		}
+	}
+}
